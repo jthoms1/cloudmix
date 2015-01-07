ece59c26c52abd9e74e9f1d49a90fcb1299748ea
Josh Thomas <jthoms1@gmail.com>
2015-01-06 23:27:16 -0600

    Added id3 tag identification.

---
+++ filldb.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var id3tagger = require('./id3');
+var glob = require('glob');
+
+// Setup database connection info
+var dbConfig = require('./config/dbc.json');
+var knex = require('knex')({
+  client: 'pg',
+  debug: true,
+  connection: {
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.dbname
+  }
+});
+var bookshelf = require('bookshelf')(knex);
+var models = require('./models')(bookshelf);
+
+glob('data/*.mp3', {}, function (err, files) {
+  files.forEach(
+});
---
+++ id3.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Promise = require('bluebird');
+var probe = Promise.promisify(require('node-ffprobe'));
+
+module.exports = function (trackPath) {
+  return probe(trackPath).then(function(probeData) {
+    var data = {};
+    data.albumArtist = {
+      name: probeData.metadata.album_artist
+    };
+    data.songArtist = {
+      name: probeData.metadata.artist
+    };
+    data.album = {
+      name: probeData.metadata.album,
+      year: probeData.metadata.date
+    };
+    data.song = {
+      name: probeData.metadata.title,
+      'album_order': probeData.metadata.track,
+      duration: probeData.format.duration
+    };
+
+    return data;
+  }).catch(function(e) {
+    console.log("Error reading file", e);
+  });
+};
